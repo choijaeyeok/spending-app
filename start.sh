7929df9ac43b845c8d238dca31e535d2dc11b5ea
choijaeyeok <52644877+choijaeyeok@users.noreply.github.com>
2026-05-06 03:08:54 +0900

    Fix start.sh: use regex to replace any existing html tag attributes

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 python -c "
-import streamlit, os
+import streamlit, os, re
 path = os.path.join(os.path.dirname(streamlit.__file__), 'static/index.html')
 html = open(path).read()
-if 'lang=\"ko\"' not in html:
-    html = html.replace('<html>', '<html lang=\"ko\" translate=\"no\">', 1)
-    open(path, 'w').write(html)
+html = re.sub(r'<html[^>]*>', '<html lang=\"ko\" translate=\"no\">', html, count=1)
+open(path, 'w').write(html)
 "
 streamlit run app.py --server.port=$PORT --server.address=0.0.0.0 --server.headless=true
